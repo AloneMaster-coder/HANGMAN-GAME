f91120305821b4da38e976238796747da6d17df2
AloneMaster-coder <akaditya0207@gmail.com>
2026-05-15 03:01:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,26 +65,6 @@ let words = [
     "bottle",
 ];
 
-let selectedWord = words[Math.floor(Math.random() * words.length)];
-
-let randomIndex = Math.floor(Math.random() * selectedWord.length);
-
-let hintLetter = selectedWord[randomIndex];
-
-let displayWord = "";
-
-for(let i = 0; i < selectedWord.length; i++) {
-
-    if(selectedWord[i] === hintLetter) {
-        displayWord += selectedWord[i] + " ";
-    } else {
-        displayWord += "_ ";
-    }
-}
-
-document.getElementById("word").innerText = displayWord;
-
-
 let computer =
 words[Math.floor(Math.random() * words.length)];
 
